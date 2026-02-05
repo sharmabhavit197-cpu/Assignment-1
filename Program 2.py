e12dd5b86987b63fb12417436f01bae60f11d0f6
sharmabhavit197-cpu <sharmabhavit197@gmail.com>
2026-02-05 21:42:05 +0530

    Add files via upload

---
+++ Program 2.py
@@ -0,0 +1,4 @@
+Enter_your_First_Name= input("Enter_your_First_Name:")
+Enter_your_last_Name= input("Enter_your_last_Name")
+print("Hello,", Enter_your_First_Name ,Enter_your_last_Name,'!', "Welcome to the python programing.")
+
