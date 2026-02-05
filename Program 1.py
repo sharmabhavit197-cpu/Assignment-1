bac1224f5606b2a2ebf0d64fad78d453e707dd9a
sharmabhavit197-cpu <sharmabhavit197@gmail.com>
2026-02-05 21:40:00 +0530

    Assignment1

---
+++ Program 1.py
@@ -0,0 +1,11 @@
+First_Number = float(input("Enter First Number: "))
+
+Second_Number = float(input("Enter Second Number: "))
+Addition= First_Number + Second_Number
+print("Addition is: ", Addition)
+Substract = First_Number - Second_Number
+print("Substract is: ", Substract)
+Multiplication = First_Number * Second_Number
+print("Multiplication is: ", Multiplication)
+Division = First_Number / Second_Number
+print("Division is: ", Division)
